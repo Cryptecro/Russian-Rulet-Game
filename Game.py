03371d29fc0cfa1df2548acfdf5189a0c9bd50a5
Zach <thesonyman101@gmail.com>
2016-05-06 16:14:35 -0700

    adding logic
    for the death system

--- Game.py
+++ Game.py
@@ -8,7 +8,13 @@ player1 = raw_input("Enter player 1 name: ")
 player2 = raw_input("Enter player 2 name: ")
 player3 = raw_input("Enter player 3 name: ")
 player4 = raw_input("Enter player 4 name: ")
-vars = [player1,player2,player3,player4]
 
-print random.sample(vars, 1)
-print "Dies"
+vars = [player1,player2,player3,player4]
+from random import randint
+print(randint(1,6))
+if randint = 6 
+  print random.sample(vars, 1)
+  print "Dies"
+else
+  print "vars,1)
+  

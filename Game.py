79d2d61474741b7cc16ebb9b123edd1bd7484801
Zach <thesonyman101@gmail.com>
2016-05-06 16:24:23 -0700

    fixed random picking of the vars

--- Game.py
+++ Game.py
@@ -8,13 +8,7 @@ player1 = raw_input("Enter player 1 name: ")
 player2 = raw_input("Enter player 2 name: ")
 player3 = raw_input("Enter player 3 name: ")
 player4 = raw_input("Enter player 4 name: ")
-
 vars = [player1,player2,player3,player4]
-from random import randint
-print(randint(1,6))
-if randint = 6 
-  print random.sample(vars, 1)
-  print "Dies"
-else
-  print "vars,1)
-  
+
+print random.choice(vars)
+print "Dies"

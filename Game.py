2c776466fb7bc5b931141e27b4bedb295e90194f
Zach <thesonyman101@gmail.com>
2016-05-06 14:08:09 -0700

    made playable game

--- Game.py
+++ Game.py
@@ -1,16 +1,14 @@
+#Status:Playable
 import sys
 import time 
 import random 
 from sys import version_info
-#making players vars
 
-#maybe add save system 
-#then learn random 
-#then profit?
 player1 = raw_input("Enter player 1 name: ")
 player2 = raw_input("Enter player 2 name: ")
 player3 = raw_input("Enter player 3 name: ")
 player4 = raw_input("Enter player 4 name: ")
+vars = [player1,player2,player3,player4]
 
-print random.choice(player1, player2, player3, player4 )
-print "you lose"
+print random.sample(vars, 1)
+print "Dies"

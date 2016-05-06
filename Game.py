2d8cfafcd67b835e9537374d405453b5583df016
Zach <thesonyman101@gmail.com>
2016-05-06 14:02:38 -0700

    Update Game.py
    Needs testing

--- Game.py
+++ Game.py
@@ -1,9 +1,16 @@
 import sys
 import time 
 import random 
-
+from sys import version_info
 #making players vars
 
 #maybe add save system 
 #then learn random 
 #then profit?
+player1 = raw_input("Enter player 1 name: ")
+player2 = raw_input("Enter player 2 name: ")
+player3 = raw_input("Enter player 3 name: ")
+player4 = raw_input("Enter player 4 name: ")
+
+print random.choice(player1, player2, player3, player4 )
+print "you lose"

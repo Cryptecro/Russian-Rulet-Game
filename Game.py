0137a2a75a9198fec2001ca99ac8e0b33d3ccc8d
Zach <thesonyman101@gmail.com>
2016-05-11 13:50:05 -0700

    Update Game.py

--- Game.py
+++ Game.py
@@ -8,7 +8,12 @@ player1 = raw_input("Enter player 1 name: ")
 player2 = raw_input("Enter player 2 name: ")
 player3 = raw_input("Enter player 3 name: ")
 player4 = raw_input("Enter player 4 name: ")
+doesshoot = 0 
 vars = [player1,player2,player3,player4]
 
 print random.choice(vars)
+if doesshoot = 0 
+  print random.choice(vars) 
+  print "lives"
+  #making vars 1/6 chance 
 print "Dies"

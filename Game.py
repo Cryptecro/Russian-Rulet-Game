7386366cdbc6b4e47778c1f55c24c4653881a12a
Zach <thesonyman101@gmail.com>
2016-05-24 13:10:02 -0700

    Update Game.py

--- Game.py
+++ Game.py
@@ -1,24 +1,26 @@
-#Status:Playable
+#Status:Kinda Playable
 import sys
 import time 
 import random 
 from sys import version_info
 
-player1 = raw_input("Enter player 1 name: ")
-player2 = raw_input("Enter player 2 name: ")
-player3 = raw_input("Enter player 3 name: ")
-player4 = raw_input("Enter player 4 name: ")
+player1 = ("Vincent")#raw_input("Enter player 1 name: ")
+player2 = ("Kyler")#raw_input("Enter player 2 name: ")
+player3 = ("Zach")#raw_input("Enter player 3 name: ")
+player4 = ("Me")#raw_input("Enter player 4 name: ")
 doesshoot = 0 
 vars = [player1,player2,player3,player4]
 Deade = random.choice(vars)
 shoot = random.randint(1,7)
   
-if shoot == 7 
-  print %Deade "Dies"
+if shoot < 5:
+    print "%s Dies" % Deade
 else:
-  print %Deade "Lives
+    print "%s Lives" % Deade 
 
-int i = 0
-for int i < 4:
-  
-  i++
+#total = 0
+#i = 1
+
+#while i <=4:
+    #total += i
+    #i += 1
